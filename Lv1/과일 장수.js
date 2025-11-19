dc587106272010dcb86585750ac1466cbd6dec48
ye1n <uyeinyein@gmail.com>
2025-11-19 16:41:43 +0900

    feat: 과일 장수 문제 정답 코드 작성

--- Lv1/과일 장수.js
+++ Lv1/과일 장수.js
@@ -1,13 +1,13 @@
-// k: 사과 품질
-// m: 한상자에 담길 사과 개수
-// score: 점수
-
-// 낮은 점수 * 개수 = 가격
-
 function solution(k, m, score) {
   var answer = 0;
+  const sortedScore = score.sort((a, b) => b - a); // 내림차순 정렬
+
+  for (let i = 0; i < Math.floor(sortedScore.length / m); i++) {
+    let min = sortedScore[i * m + m - 1];
+    answer += min * m;
+  }
 
   return answer;
 }
 
-console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
+console.log(solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]));

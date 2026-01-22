8b4a7e97c29d8181d35d8af2dd8fe98fcd9ce37b
ye1n <uyeinyein@gmail.com>
2026-01-22 14:50:18 +0900

    feat: [소수 찾기] 문제 풀이중

--- Lv1/소수 찾기.js
+++ Lv1/소수 찾기.js
@@ -4,6 +4,17 @@
 function solution(n) {
   let answer = 0;
 
+  const isPrime = (num) => {
+    for (let i = 2; i * i <= num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) answer++;
+  }
+
   return answer;
 }
 

48f1c10ed7d63d6fc2df97cdd3113f0fcd80e7ac
ye1n <uyeinyein@gmail.com>
2026-01-27 17:53:33 +0900

    feat: [체육복] 정답 코드

--- Lv1/체육복.js
+++ Lv1/체육복.js
@@ -2,26 +2,27 @@
  * 체육복
  */
 function solution(n, lost, reserve) {
-  let answer = n;
   let lostIndex = new Set(lost.sort());
   let reserveIndex = new Set(reserve.sort());
 
-  for (const index of lostIndex) {
-    if (reserveIndex.has(index)) {
-      reserveIndex.delete(index);
-      lostIndex.delete(index);
-    } else if (reserveIndex.has(index - 1)) {
-      reserveIndex.delete(index - 1);
-      lostIndex.delete(index);
-    } else if (reserveIndex.has(index + 1)) {
-      reserveIndex.delete(index + 1);
-      lostIndex.delete(index);
+  for (const i of lostIndex) {
+    if (reserveIndex.has(i)) {
+      reserveIndex.delete(i);
+      lostIndex.delete(i);
     }
   }
 
-  answer -= lostIndex.size;
+  for (const i of lostIndex) {
+    if (reserveIndex.has(i - 1)) {
+      reserveIndex.delete(i - 1);
+      lostIndex.delete(i);
+    } else if (reserveIndex.has(i + 1)) {
+      reserveIndex.delete(i + 1);
+      lostIndex.delete(i);
+    }
+  }
 
-  return answer;
+  return (n -= lostIndex.size);
 }
 
-console.log(solution(5, [2, 3], [3, 4])); // 4
+console.log(solution(5, [2, 3], [3, 4]));

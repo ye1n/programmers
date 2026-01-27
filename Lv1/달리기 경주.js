df6b297ddd1795290dd11f98096e2cc81f9093e8
ye1n <uyeinyein@gmail.com>
2026-01-27 15:34:12 +0900

    refactor: 불필요 주석 제거

--- Lv1/달리기 경주.js
+++ Lv1/달리기 경주.js
@@ -1,31 +1,22 @@
 /**
  * 달리기 경주
- * @param {*} players
- * @param {*} callings
- * @returns
  */
 function solution(players, callings) {
   const answer = [...players];
-  const rank = new Map(); // 등수 저장
+  const rank = new Map();
 
-  // rank = { 'mumu' => 0, 'soe' => 1, 'poe' => 2, 'kai' => 3, 'mine' => 4 }
-  // answer = [ 'mumu', 'soe', 'poe', 'kai', 'mine' ]
   for (let i = 0; i < answer.length; i++) {
     const x = answer[i];
     rank.set(x, i);
   }
 
-  // callings = ["kai", "kai", "mine", "mine"]
   for (const x of callings) {
-    // x = 'kai'
-    let nextRank = rank.get(x); // 3
-    let prevRank = nextRank - 1; // 2
+    let nextRank = rank.get(x);
+    let prevRank = nextRank - 1;
 
-    // rank = { 'mumu' => 0, 'soe' => 1, 'poe' => 3, 'kai' => 2, 'mine' => 4 }
     rank.set(answer[prevRank], nextRank);
     rank.set(answer[nextRank], prevRank);
 
-    // answer = [ 'mumu', 'soe', 'kai', 'poe', 'mine' ]
     [answer[prevRank], answer[nextRank]] = [answer[nextRank], answer[prevRank]];
   }
 
@@ -35,6 +26,6 @@ function solution(players, callings) {
 console.log(
   solution(
     ["mumu", "soe", "poe", "kai", "mine"],
-    ["kai", "kai", "mine", "mine"]
-  )
+    ["kai", "kai", "mine", "mine"],
+  ),
 );

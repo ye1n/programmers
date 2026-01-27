f880db851f45687cf66337972bd5c1bcc2315f81
ye1n <uyeinyein@gmail.com>
2026-01-27 15:17:39 +0900

    feat: [덧칠하기] 정답코드 작성

--- Lv1/덧칠하기.js
+++ Lv1/덧칠하기.js
@@ -2,7 +2,15 @@
  * 덧칠하기
  */
 function solution(n, m, section) {
-  let answer = 0; // 2
+  let answer = 0;
+  let currentIndex = 0;
+
+  for (const nextWall of section) {
+    if (currentIndex <= nextWall) {
+      currentIndex = nextWall + m;
+      answer++;
+    }
+  }
 
   return answer;
 }

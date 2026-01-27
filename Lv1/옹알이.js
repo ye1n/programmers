dd3eb90f0d52437aa842a02909d3a06eb3ed21a9
ye1n <uyeinyein@gmail.com>
2026-01-27 11:29:06 +0900

    feat: [옹알이] 정답코드 작성

--- Lv1/옹알이.js
+++ Lv1/옹알이.js
@@ -3,8 +3,25 @@
  */
 function solution(babbling) {
   let answer = 0;
+  let canSayWord = ["aya", "ye", "woo", "ma"];
+
+  for (const word of babbling) {
+    let temp = "";
+    let before = "";
+
+    for (const char of word) {
+      temp += char;
+
+      if (temp !== before && canSayWord.includes(temp)) {
+        before = temp;
+        temp = "";
+      }
+    }
+
+    if (!temp.length) answer++;
+  }
 
   return answer;
 }
 
-console.log(solution(["aya", "yee", "u", "maa"]));
+console.log(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]));

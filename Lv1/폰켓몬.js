11d050758b628c9e7e0395ff32efbc9610ab0cdb
ye1n <uyeinyein@gmail.com>
2026-01-27 16:14:51 +0900

    refactor: [폰켓몬] 개선

--- Lv1/폰켓몬.js
+++ Lv1/폰켓몬.js
@@ -3,9 +3,9 @@
  */
 function solution(nums) {
   let canPick = nums.length / 2;
-  let kinds = new Set(nums);
+  let kinds = new Set(nums).size;
 
-  return canPick <= kinds.size ? canPick : kinds.size;
+  return canPick <= kinds ? canPick : kinds;
 }
 
 console.log(solution([3, 1, 2, 3]));

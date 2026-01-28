85a6751ef4c3986d4fa7572dd604b12947d470e6
ye1n <uyeinyein@gmail.com>
2026-01-28 09:23:07 +0900

    refactor: [둘만의 암호] 개선

--- Lv1/둘만의 암호.js
+++ Lv1/둘만의 암호.js
@@ -2,12 +2,8 @@
  * 둘만의 암호
  */
 function solution(s, skip, index) {
-  let answer = "";
-  let sIdx = [];
-  let skipIdx = [];
-
-  sIdx = s.split("").map((str) => str.charCodeAt(0) - 97);
-  skipIdx = skip.split("").map((str) => str.charCodeAt(0) - 97);
+  let sIdx = s.split("").map((str) => str.charCodeAt(0) - 97);
+  let skipIdx = skip.split("").map((str) => str.charCodeAt(0) - 97);
 
   for (let i = 0; i < sIdx.length; i++) {
     let cnt = index;
@@ -17,9 +13,7 @@ function solution(s, skip, index) {
     }
   }
 
-  answer = sIdx.map((i) => String.fromCharCode(97 + i)).join("");
-
-  return answer;
+  return sIdx.map((i) => String.fromCharCode(97 + i)).join("");
 }
 
 console.log(solution("aukks", "wbqd", 5));

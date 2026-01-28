f978663daf6257826ce4231d9ac3c0853210ea29
ye1n <uyeinyein@gmail.com>
2026-01-28 14:48:40 +0900

    refactor: [기사단원의 무기] 개선

--- Lv1/기사단원의 무기.js
+++ Lv1/기사단원의 무기.js
@@ -5,16 +5,17 @@ function solution(number, limit, power) {
   let weight = 0;
 
   for (let i = 1; i <= number; i++) {
-    let cnt = new Set();
+    let cnt = 0;
 
     for (let j = 1; j * j <= i; j++) {
       if (i % j === 0) {
-        cnt.add(j);
-        cnt.add(i / j);
+        cnt++;
+
+        if (j !== i / j) cnt++;
       }
     }
 
-    weight += cnt.size > limit ? power : cnt.size;
+    weight += cnt > limit ? power : cnt;
   }
 
   return weight;

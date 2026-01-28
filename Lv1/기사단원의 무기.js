1336b1cc0dafa72831f5d9656fbd17874e1c85d6
ye1n <uyeinyein@gmail.com>
2026-01-28 13:53:35 +0900

    feat: [기사단원의 무기] 정답 코드

--- Lv1/기사단원의 무기.js
+++ Lv1/기사단원의 무기.js
@@ -2,8 +2,22 @@
  * 기사단원의 무기
  */
 function solution(number, limit, power) {
-  var answer = 0;
-  return answer;
+  let weight = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let cnt = new Set();
+
+    for (let j = 1; j * j <= i; j++) {
+      if (i % j === 0) {
+        cnt.add(j);
+        cnt.add(i / j);
+      }
+    }
+
+    weight += cnt.size > limit ? power : cnt.size;
+  }
+
+  return weight;
 }
 
-console.log(solution(5, 3, 2));
+console.log(solution(10, 3, 2));

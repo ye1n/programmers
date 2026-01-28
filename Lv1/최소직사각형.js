8fbf8bfb40ef6e9d9ecf72df0f2e9c2622725b5a
ye1n <uyeinyein@gmail.com>
2026-01-28 18:01:21 +0900

    feat: [최소직사각형] 문제 파일

---
+++ Lv1/최소직사각형.js
@@ -0,0 +1,16 @@
+/**
+ * 최소직사각형
+ */
+function solution(sizes) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(
+  solution([
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40],
+  ]),
+);

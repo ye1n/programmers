571cefe75d724f042c079ef4c43997b05594d5e6
ye1n <uyeinyein@gmail.com>
2026-02-03 18:12:38 +0900

    feat: [카펫] 정답코드 작성

--- Lv2/카펫.js
+++ Lv2/카펫.js
@@ -2,7 +2,16 @@
  * 카펫
  */
 function solution(brown, yellow) {
-  return [];
+  let carpets = brown + yellow;
+  let h;
+  let w;
+
+  for (h = 3; h <= carpets / h; h++) {
+    w = carpets / h;
+    if (carpets % h === 0 && yellow === (w - 2) * (h - 2)) break;
+  }
+
+  return [w, h];
 }
 
 console.log(solution(10, 2));

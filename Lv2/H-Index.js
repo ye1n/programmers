477755d257d3200a50070719bac9afe847e6f4cd
ye1n <uyeinyein@gmail.com>
2026-02-06 15:58:57 +0900

    refactor: [H-Index] 정렬 사용

--- Lv2/H-Index.js
+++ Lv2/H-Index.js
@@ -2,10 +2,11 @@
  * H-Index
  */
 function solution(citations) {
+  citations.sort((a, b) => b - a);
   let h = citations.length;
 
-  for (h; h > -1; h--) {
-    if (citations.filter((c) => c >= h).length >= h) break;
+  for (h; h > 0; h--) {
+    if (citations[h - 1] >= h) break;
   }
 
   return h;
